a13d011f764528f611ee285f274795390876ac95
Avner Biblarz <abiblarz@csumb.edu>
2017-07-26 17:37:46 -0700

    jquery day

--- main.js
+++ main.js
@@ -1,7 +1,28 @@
 // console.log('js file is connected!');
 // console.log(2 + 2);
-
 // var studentNames = ['Fred', 'Susie', 'Roberto'];
+
+// var test = $('#pink-heading');
+// test.text('I H8 TENN1S');
+
+function myGreeting() {
+  console.log('button was clicked');
+  var userName = $('#username').val();
+  console.log(userName);
+  var userAge = $('#userage').val();
+  console.log(userAge);
+  $('h1').text('Welcome, ' + userName);
+}
+
+function setup() {
+  console.log('page is ready');
+  $('#submitButton').click(myGreeting);
+}
+
+$(document).ready(setup);
+
+
+
 
 function checkout (item1, item2, coupon) {
   var subtotal = item1 + item2;
@@ -11,30 +32,46 @@ function checkout (item1, item2, coupon) {
   return grandTotal;
 }
 
-function helloFunction(name) {
-  if(name === 'Sammy') {
-    console.log('Go away. I h8 you.')
-  } else {
-    console.log('Hello ' + name + '!');
-  }
-}
 
-helloFunction('sammy');
+// console.log(checkout(-50, 30));
 
-console.log(checkout(33, 19, .15));
 
-var oneItemResult = checkout(12, 0, 0);
 
-console.log(oneItemResult);
 
-checkout(500, 1000, .05);
 
-var bananaPrice = 1.5;
-var chocolatePudding = 6;
-var superCoupon = .45;
 
-var bankAccount = 100; // :(
 
-var remainAmount = bankAccount - checkout(bananaPrice, chocolatePudding, superCoupon);
 
-console.log('I have this left in my bank account: ' + remainAmount + '.');
+
+
+
+
+
+// function helloFunction(name) {
+//   if(name === 'Sammy') {
+//     console.log('Go away. I h8 you.')
+//   } else {
+//     console.log('Hello ' + name + '!');
+//   }
+// }
+//
+// helloFunction('sammy');
+//
+// console.log(checkout(33, 19, .15));
+//
+// var oneItemResult = checkout(12, 0, 0);
+//
+// console.log(oneItemResult);
+//
+// checkout(500, 1000, .05);
+//
+// var bananaPrice = 1.5;
+// var chocolatePudding = 6;
+// chocolatePudding = 7;
+// var superCoupon = .45;
+//
+// var bankAccount = 100; // :(
+//
+// var remainAmount = bankAccount - checkout(bananaPrice, chocolatePudding, superCoupon);
+//
+// console.log('I have this left in my bank account: ' + remainAmount + '.');
